f9d418bfcabdbd3b883855f507514f6c384bae2c
anashamdiosman <anas-el-tom@live.com>
2024-05-27 22:44:25 +0400

    new favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Other tags like title, meta can also go here */}
+        <link rel="icon" href="/assets/ah.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
